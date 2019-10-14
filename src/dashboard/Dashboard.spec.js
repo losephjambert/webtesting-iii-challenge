bfd9b8f89e1ea42a44a39c32a61eb1f5642669cb
Joe <josephc.lambert@gmail.com>
2019-10-14 15:20:53 -0700

    adds basic render test to all components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('it renders correctly', () => {
+  // do snapshots here if you're not in codesandbox
+  render(<Dashboard />);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,7 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Display from './Display';
+
+test('Display renders correctly', () => {
+  render(<Display />);
+});
