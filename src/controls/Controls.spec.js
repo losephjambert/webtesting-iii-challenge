cbf9a511948e5c88090c2f2594f544e2c41114cd
Joe <josephc.lambert@gmail.com>
2019-10-14 16:10:37 -0700

    adds test for unlocked and closed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -18,3 +18,16 @@ test('Gate Locked and Closed returns can be unlocked, cannot be opened', () => {
   fireEvent.click(getByTestId('gate-button'));
   expect(toggleClosedMockSpy).toHaveBeenCalledTimes(0);
 });
+
+test('Gate Unlocked and Closed returns can be locked, can be opened', () => {
+  const toggleLockedMockSpy = jest.fn();
+  const toggleClosedMockSpy = jest.fn();
+
+  const { getByTestId } = render(
+    <Controls locked={false} closed={true} toggleLocked={toggleLockedMockSpy} toggleClosed={toggleClosedMockSpy} />
+  );
+  fireEvent.click(getByTestId('lock-button'));
+  expect(toggleLockedMockSpy).toHaveBeenCalledTimes(1);
+  fireEvent.click(getByTestId('gate-button'));
+  expect(toggleClosedMockSpy).toHaveBeenCalledTimes(1);
+});
