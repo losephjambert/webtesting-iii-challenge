6e9fece4a0948958b94f5247b0366fb6db2a2d7c
Joe <josephc.lambert@gmail.com>
2019-10-15 07:34:23 -0700

    adds test suite for Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,3 +6,13 @@ test('it renders correctly', () => {
   // do snapshots here if you're not in codesandbox
   render(<Dashboard />);
 });
+
+test('gate should default to unlocked and open', () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText(/open/i));
+  expect(getByText(/unlocked/i));
+});
+
+test('Dashboard should render Display and Controls', () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
