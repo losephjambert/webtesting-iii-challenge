bc66422fc733607ec54fb38c1d5d9ad08e07d562
Joe <josephc.lambert@gmail.com>
2019-10-15 07:38:22 -0700

    display component test suite.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,24 +1,28 @@
-import React from "react";
-import { render, queryByText } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Display from "./Display";
+import React from 'react';
+import { render, queryByText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
 
-test("Display renders correctly", () => {
+test('Display renders correctly', () => {
   render(<Display />);
 });
 
-//locked
-//closed
-
-test("Display locked if locked prop is true. Display unlocked if locked prop is false", () => {
+test('Display locked if locked prop is true. Display unlocked if locked prop is false', () => {
   const { getByText, queryByText } = render(<Display locked={false} />);
   expect(getByText(/unlocked/i));
   expect(queryByText(/^locked/i)).toBeFalsy();
 });
 
 test("Class should be 'green-led' if unlocked or open", () => {
-  render(<Display locked={false} />);
+  const { queryByText } = render(<Display locked={false} />);
   const lock = queryByText(/unlocked/i);
   expect(lock).toBeInTheDocument();
-  expect(lock).toHaveClass("green-led");
+  expect(lock).toHaveClass('green-led');
+});
+
+test("Class should be 'red-led' if unlocked or open", () => {
+  const { queryByText } = render(<Display locked={true} />);
+  const lock = queryByText(/locked/i);
+  expect(lock).toBeInTheDocument();
+  expect(lock).toHaveClass('red-led');
 });
